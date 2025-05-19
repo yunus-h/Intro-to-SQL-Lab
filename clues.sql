fc7f958d4cefe5ba429c46c304f7341c00eede81
Yunus <yrherman8@gmail.com>
2025-05-19 02:30:51 -0400

    updating some files

--- clues.sql
+++ clues.sql
@@ -2,32 +2,63 @@
  
 -- Write SQL query here
 
+\echo Clue #1:
+
+SELECT name, population, region
+FROM countries
+WHERE region = 'Southern Europe'
+ORDER BY population ASC
+LIMIT 1;
 
 -- Clue #2: Now that we're here, we have insight that Carmen was seen attending language classes in this country's officially recognized language. Check our databases and find out what language is spoken in this country, so we can call in a translator to work with you.
 
 -- Write SQL query here
 
+\echo Clue #2:
+
+SELECT language
+FROM countries JOIN countrylanguages ON countries.code = countrylanguages.countrycode
+WHERE countries.name LIKE '%Vatican%';
 
 -- Clue #3: We have new news on the classes Carmen attended – our gumshoes tell us she's moved on to a different country, a country where people speak only the language she was learning. Find out which nearby country speaks nothing but that language.
 
 -- Write SQL query here
 
+\echo Clue #3:
+
+SELECT name, language
+FROM countries JOIN countrylanguages ON countries.code = countrylanguages.countrycode
+WHERE language = 'Italian' AND region = 'Southern Europe' AND name LIKE '%Vatican%';
+
+
 
 -- Clue #4: We're booking the first flight out – maybe we've actually got a chance to catch her this time. There are only two cities she could be flying to in the country. One is named the same as the country – that would be too obvious. We're following our gut on this one; find out what other city in that country she might be flying to.
 
 -- Write SQL query here
+\echo Clue #4:
 
+SELECT cities.name 
+FROM cities JOIN countries ON cities.countrycode = countries.code
+WHERE countries.name = 'San Marino' AND cities.name != countries.name;
 
 -- Clue #5: Oh no, she pulled a switch – there are two cities with very similar names, but in totally different parts of the globe! She's headed to South America as we speak; go find a city whose name is like the one we were headed to, but doesn't end the same. Find out the city, and do another search for what country it's in. Hurry!
 
 -- Write SQL query here
+\echo Clue #5:
 
+SELECT cities.name, countries.name
+FROM cities JOIN countries ON cities.countrycode = countries.code
+WHERE region = 'South America' AND cities.name LIKE 'Serra%';
 
 -- Clue #6: We're close! Our South American agent says she just got a taxi at the airport, and is headed towards
 -- the capital! Look up the country's capital, and get there pronto! Send us the name of where you're headed and we'll
 -- follow right behind you!
 
 -- Write SQL query here
+\echo Clue #6:
+SELECT cities.name, countries.name
+FROM cities JOIN countries ON cities.countrycode = countries.code
+WHERE countries.name = 'Brazil' AND cities.id = countries.capital;
 
 
 -- Clue #7: She knows we're on to her – her taxi dropped her off at the international airport, and she beat us to the boarding gates. We have one chance to catch her, we just have to know where she's heading and beat her to the landing dock. Lucky for us, she's getting cocky. She left us a note (below), and I'm sure she thinks she's very clever, but if we can crack it, we can finally put her where she belongs – behind bars.
@@ -42,3 +73,11 @@
 
 
 -- We're counting on you, gumshoe. Find out where she's headed, send us the info, and we'll be sure to meet her at the gates with bells on.
+
+\echo Clue #7:
+
+SELECT cities.name, cities.population, countries.name
+FROM cities JOIN countries ON cities.countrycode = countries.code
+WHERE cities.population = 91000;
+
+\echo "She is in Najran, Saudi Arabia beacuse it's sunnier than Krasnogorsk"
